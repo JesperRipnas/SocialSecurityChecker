eb52553f2a29b78c79b5a924bc915a5035c23acc
Rippan <jepperipnas@gmail.com>
2020-10-26 18:49:51 +0100

    minor bugfix

--- Program.cs
+++ Program.cs
@@ -90,13 +90,7 @@ namespace SocialSecurityChecker
                 int day = Convert.ToInt32(userInput.Substring(6, 2));
                 switch (month)
                 {
-                    case 1:
-                    case 3:
-                    case 5:
-                    case 7:
-                    case 8:
-                    case 10:
-                    case 12:
+                    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
                         if (day >= 1 && day <= 31)
                         {
                             return true;
@@ -106,10 +100,7 @@ namespace SocialSecurityChecker
                             Console.WriteLine($"(!) Incorrect day ({day}), try again!");
                         }
                         break;
-                    case 4:
-                    case 6:
-                    case 9:
-                    case 11:
+                    case 4: case 6: case 9: case 11:
                         if (day >= 1 && day <= 30)
                         {
                             return true;
